41052253cd0fc4677edf1ec5fea3b16e9ce8bf4a
tecnologyboy <lps76@hotmail.com>
2023-03-15 01:07:55 -0400

    peliculas app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Peliculas',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'detail': (_) => const DetailScreen(),
+      },
+    );
+  }
+}
