3ed2d8e28b66824f4b5bb0bb82135e61701e29a9
tecnologyboy <lps76@hotmail.com>
2023-03-21 16:23:10 -0400

    se agrego el widget de resumen de la pelicula

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -14,6 +14,11 @@ class DetailScreen extends StatelessWidget {
         body: CustomScrollView(
       slivers: [
         _CustomAppbar(),
+        SliverList(
+          delegate: SliverChildListDelegate.fixed([
+            _PosterAndtitle(),
+          ]),
+        )
       ],
     ));
   }
@@ -44,6 +49,66 @@ class _CustomAppbar extends StatelessWidget {
               'https://www.mountaineers.org/images/placeholder-images/placeholder-300-x-400'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndtitle extends StatelessWidget {
+  const _PosterAndtitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage('https://dummyimage.com/200x300/'),
+                height: 150,
+              )),
+          const SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headlineMedium,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.title',
+                style: textTheme.titleLarge,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star_outline,
+                    size: 15,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'movie-vote.average',
+                    style: textTheme.bodySmall,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
